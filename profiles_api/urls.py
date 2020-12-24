53ae1a8cb4b9cccc2b7005999545447079a62e47
Evgenii <lakone@bisoft.fi>
2020-12-24 19:25:23 +0200

    Added viewsets to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
+
+
+router = DefaultRouter()
+router.register('hw-viewset', views.HWViewSet, base_name='hw-viewset')
 
 
 urlpatterns = [
     path('HW-view/', views.HWApiView.as_view()),
+    path('', include(router.urls)),
 ]
